356cba49b251a4d35bb898f1cffc91dbe9b20a62
oivheg <oivheg@gmail.com>
2013-11-13 19:19:56 +0100

    added physics to House through Moc

--- src/Moc/Mocs.java
+++ src/Moc/Mocs.java
@@ -5,6 +5,9 @@
 package Moc;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.PointLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -23,6 +26,8 @@ public class Mocs {
       Box box1 = new Box(1,1,1);
      Geometry blue = new Geometry("Box",box1);
 blue.setLocalTranslation(new Vector3f(x,1f,0));
+blue.addControl((new RigidBodyControl( 1.0f )));
+
 Material mat1 = new Material(manager,
         "Common/MatDefs/Misc/Unshaded.j3md");
 mat1.setColor("Color",ColorRGBA.Blue);
